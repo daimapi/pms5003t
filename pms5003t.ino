cf0f8e92a88b0acf97fd60fbbc7c14b3562bd788
daimapi <unduainspacepig0302@gmail.com>
2023-07-19 17:36:48 +0800

    first commit

---
+++ pms5003t.ino
@@ -0,0 +1,33 @@
+#include <PMS.h> 
+#include <SoftwareSerial.h> 
+
+SoftwareSerial Serial1(2, 3); // 將Arduino Pin2設定為RX, Pin3設定為TX
+PMS pms(Serial1);
+PMS::DATA data;
+
+void setup()
+{
+  Serial.begin(9600);   
+  Serial1.begin(9600);//for pms
+  pms.passiveMode();
+}
+void loop()
+{
+  
+  if (Serial.available()>0 && Serial.read()=='1')
+  {
+    Serial.println("o");
+    pms.wakeUp();
+    delay(30000);
+    pms.requestRead();
+    if (pms.readUntil(data))
+    {
+      Serial.println(data.PM_AE_UG_1_0);
+      Serial.println(data.PM_AE_UG_2_5);
+      Serial.println(data.PM_AE_UG_10_0);
+    }
+    delay(1000);
+    pms.sleep();
+    Serial.println("d");
+  }
+}
